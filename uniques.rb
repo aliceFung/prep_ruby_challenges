9727ffb6e7fd77727e608bcab97982b255b87320
--replace-all <krystallattice@gmail.com>
2015-05-12 16:12:25 +0000

    fixed loop w break

--- uniques.rb
+++ uniques.rb
@@ -2,19 +2,20 @@
 
 def uniques(array)
     unique_array=[]
-    copy = false
     array.each do |element|
         unique_array.each { |uniq| 
             if uniq==element
-                copy = true 
+                @copy = true
+                break
             else
-                copy = false
+                @copy = false
             end
             }
-        unique_array << element unless copy
+        unique_array << element unless @copy
     end
     print unique_array
 end 
 
-uniques([1,5,"frog", 2,1,3,"frog"])
-#not working yet; 12AM EST 5.12/15
+#uniques( [ 1, 1,1,1,4, 2, 7, 3, 1, 2, 8,1] )
+
+#uniques([1,5,"frog", 2,1,3,"frog"])
