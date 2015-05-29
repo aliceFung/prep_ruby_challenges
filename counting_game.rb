4fd58c0060f45b5b01e866f9ca9841da699f7cf5
Alice Fung <alice.o.fung@gmail.com>
2015-05-28 22:00:38 -0400

    simplify player array and readability

--- counting_game.rb
+++ counting_game.rb
@@ -1,35 +1,28 @@
-def players(number)
-	group=[]
-	1.upto(number) do |player|
-		group << player
-	end
-	return group
-end
-
 def counting_game(num_players, max_num)
-    group=players(num_players)
-	player_index=0
+  group = (1..100).to_a
+	player_index = 0
+
 	1.upto(max_num) {|count|
 	    puts "Player #{group[player_index].to_s} says #{count.to_s}"
 	#reverse direction if divisible by 7. i.e. 8 would be said by person8
-	    if count%7==0 && count%11==0
-		    current_player=group[player_index]
+	    if count % 7 == 0 && count % 11 == 0
+		    current_player = group[player_index]
 		    group.reverse!
-		    player_index=group.index(current_player)
-		    player_index+=2
-	    elsif count%7==0
-		    current_player=group[player_index]
+		    player_index = group.index(current_player)
+		    player_index += 2
+	    elsif count % 7 == 0
+		    current_player = group[player_index]
 		    group.reverse!
-		    player_index=group.index(current_player)
-		    player_index+=1
+		    player_index = group.index(current_player)
+		    player_index += 1
 	#skip the next person, p3 says 33, p5 says 34
-	    elsif count%11==0
-		    player_index+=2
+	    elsif count % 11 == 0
+		    player_index += 2
 	    else
-		    player_index+=1
+		    player_index += 1
 	    end
-        
-	    player_index-=group.length if player_index >= group.length
+
+	    player_index -= group.length if player_index >= group.length
 
         }
     puts "End of Game."
